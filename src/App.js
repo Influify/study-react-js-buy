925b8ab8337aecd2eca4fc407333c1cf188967e8
LJEsp <esparragoleandroj@gmail.com>
2019-02-08 22:57:25 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import store from "./store";
 
+import Cart from "./components/shopify/Cart";
+import Products from "./components/shopify/Products";
 class App extends Component {
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.addVariantToCart = this.addVariantToCart.bind(this);
+  }
+
+  updateQuantityInCart(lineItemId, quantity) {
+    const state = store.getState();
+    const checkoutId = state.checkout.id;
+    const lineItemsToUpdate = [
+      { id: lineItemId, quantity: parseInt(quantity, 10) }
+    ];
+
+    state.client.checkout
+      .updateLineItems(checkoutId, lineItemsToUpdate)
+      .then(res => {
+        store.dispatch({
+          type: "UPDATE_QUANTITY_IN_CART",
+          payload: { checkout: res }
+        });
+      });
+  }
+
+  removeLineItemInCart(lineItemId) {
+    const state = store.getState();
+    const checkoutId = state.checkout.id;
+    state.client.checkout
+      .removeLineItems(checkoutId, [lineItemId])
+      .then(res => {
+        store.dispatch({
+          type: "REMOVE_LINE_ITEM_IN_CART",
+          payload: { checkout: res }
+        });
+      });
+  }
+
+  handleCartClose() {
+    store.dispatch({ type: "CLOSE_CART" });
+  }
+
+  handleCartOpen() {
+    store.dispatch({ type: "OPEN_CART" });
+  }
+
+  addVariantToCart(variantId, quantity) {
+    const state = store.getState();
+    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }];
+    const checkoutId = state.checkout.id;
+
+    state.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
+      store.dispatch({
+        type: "ADD_VARIANT_TO_CART",
+        payload: { isCartOpen: true, checkout: res }
+      });
+    });
+  }
+
   render() {
+    const state = store.getState();
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Cart
+          checkout={state.checkout}
+          isCartOpen={state.isCartOpen}
+          handleCartClose={this.handleCartClose}
+          updateQuantityInCart={this.updateQuantityInCart}
+          removeLineItemInCart={this.removeLineItemInCart}
+        />
+
+        <Products
+          products={state.products}
+          client={state.client}
+          addVariantToCart={this.addVariantToCart}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => state)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Client from "shopify-buy";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = Client.buildClient({
+  storefrontAccessToken: "0bc41510d898fcd578c212e350fa1a49",
+  domain: "ljesp-study.myshopify.com"
+});
+
+store.dispatch({ type: "CLIENT_CREATED", payload: client });
+
+client.product.fetchAll().then(res => {
+  store.dispatch({ type: "PRODUCTS_FOUND", payload: res });
+});
+
+client.checkout.create().then(res => {
+  console.log(res);
+  store.dispatch({ type: "CHECKOUT_FOUND", payload: res });
+});
+
+client.shop.fetchInfo().then(res => {
+  store.dispatch({ type: "SHOP_FOUND", payload: res });
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
